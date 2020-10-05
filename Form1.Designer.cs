0c871fadb1acec2550e1c098ac5643f9eb9d50bf
Alina Ershova <omnom0o@mail.ru>
2020-10-05 15:54:40 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCreateFile = new System.Windows.Forms.Button();
+            this.buttonCreateFolder = new System.Windows.Forms.Button();
+            this.buttonDearchive = new System.Windows.Forms.Button();
+            this.buttonArchive = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.buttonRename = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(3, 3);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(301, 266);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "baseline_folder_black_18dp.png");
+            this.imageList1.Images.SetKeyName(1, "baseline_description_black_18dp.png");
+            this.imageList1.Images.SetKeyName(2, "baseline_business_center_black_18dp.png");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonOpen);
+            this.panel1.Controls.Add(this.buttonOK);
+            this.panel1.Controls.Add(this.buttonCreateFile);
+            this.panel1.Controls.Add(this.buttonCreateFolder);
+            this.panel1.Controls.Add(this.buttonDearchive);
+            this.panel1.Controls.Add(this.buttonArchive);
+            this.panel1.Controls.Add(this.buttonDelete);
+            this.panel1.Controls.Add(this.buttonCopy);
+            this.panel1.Controls.Add(this.buttonRename);
+            this.panel1.Location = new System.Drawing.Point(309, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(126, 266);
+            this.panel1.TabIndex = 1;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Enabled = false;
+            this.buttonOK.Location = new System.Drawing.Point(-1, 240);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(124, 23);
+            this.buttonOK.TabIndex = 7;
+            this.buttonOK.Text = "ОК";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCreateFile
+            // 
+            this.buttonCreateFile.Enabled = false;
+            this.buttonCreateFile.Location = new System.Drawing.Point(-1, 211);
+            this.buttonCreateFile.Name = "buttonCreateFile";
+            this.buttonCreateFile.Size = new System.Drawing.Size(124, 23);
+            this.buttonCreateFile.TabIndex = 6;
+            this.buttonCreateFile.Text = "Cоздать файл";
+            this.buttonCreateFile.UseVisualStyleBackColor = true;
+            // 
+            // buttonCreateFolder
+            // 
+            this.buttonCreateFolder.Enabled = false;
+            this.buttonCreateFolder.Location = new System.Drawing.Point(-1, 181);
+            this.buttonCreateFolder.Name = "buttonCreateFolder";
+            this.buttonCreateFolder.Size = new System.Drawing.Size(124, 23);
+            this.buttonCreateFolder.TabIndex = 5;
+            this.buttonCreateFolder.Text = "Создать папку\r\n";
+            this.buttonCreateFolder.UseVisualStyleBackColor = true;
+            // 
+            // buttonDearchive
+            // 
+            this.buttonDearchive.Enabled = false;
+            this.buttonDearchive.Location = new System.Drawing.Point(-1, 151);
+            this.buttonDearchive.Name = "buttonDearchive";
+            this.buttonDearchive.Size = new System.Drawing.Size(124, 23);
+            this.buttonDearchive.TabIndex = 4;
+            this.buttonDearchive.Text = "Разархивировать";
+            this.buttonDearchive.UseVisualStyleBackColor = true;
+            // 
+            // buttonArchive
+            // 
+            this.buttonArchive.Enabled = false;
+            this.buttonArchive.Location = new System.Drawing.Point(1, 93);
+            this.buttonArchive.Name = "buttonArchive";
+            this.buttonArchive.Size = new System.Drawing.Size(124, 23);
+            this.buttonArchive.TabIndex = 3;
+            this.buttonArchive.Text = "Архивировать";
+            this.buttonArchive.UseVisualStyleBackColor = true;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Enabled = false;
+            this.buttonDelete.Location = new System.Drawing.Point(1, 63);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(124, 23);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Удалить";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Enabled = false;
+            this.buttonCopy.Location = new System.Drawing.Point(1, 33);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(124, 23);
+            this.buttonCopy.TabIndex = 1;
+            this.buttonCopy.Text = "Копировать";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // buttonRename
+            // 
+            this.buttonRename.Enabled = false;
+            this.buttonRename.Location = new System.Drawing.Point(1, 4);
+            this.buttonRename.Name = "buttonRename";
+            this.buttonRename.Size = new System.Drawing.Size(124, 23);
+            this.buttonRename.TabIndex = 0;
+            this.buttonRename.Text = "Переименовать\r\n";
+            this.buttonRename.UseVisualStyleBackColor = true;
+            this.buttonRename.Click += new System.EventHandler(this.buttonRename_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(441, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 266);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.treeView1);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(750, 275);
+            this.panel2.TabIndex = 3;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Enabled = false;
+            this.buttonOpen.Location = new System.Drawing.Point(1, 122);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(124, 23);
+            this.buttonOpen.TabIndex = 8;
+            this.buttonOpen.Text = "Открыть";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 282);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Недопроводник";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonCreateFolder;
+        private System.Windows.Forms.Button buttonDearchive;
+        private System.Windows.Forms.Button buttonArchive;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.Button buttonRename;
+        private System.Windows.Forms.Button buttonCreateFile;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo root;
+        FileSystemWatcher watcher;
+        int operation;
+        TreeNode currentNode;
+
+        delegate void D();
+        public Form1()
+        {
+            InitializeComponent();
+            root = new DirectoryInfo(@"D:\code\c#\FileExplorer\Папка");
+            //System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            treeView1.ImageList = imageList1;
+            SetTreeNode();
+            SetWatcher();
+            ResetButtons();
+
+
+        }
+        void SetWatcher()
+        {
+            watcher = new FileSystemWatcher(root.FullName);
+            watcher.Changed += Handler;
+            watcher.Renamed += Handler;
+            watcher.Deleted += Handler;
+            watcher.Changed += Handler;
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        void Handler(object o, FileSystemEventArgs e)
+        {
+            treeView1.Invoke(new D(SetTreeNode));
+        }
+        void SetTreeNode()
+        {
+            treeView1.Nodes.Clear();
+            var rootNode = new TreeNode("", 0, 0);
+            rootNode.Tag = root.FullName;
+            AddBranch(rootNode, root);
+            treeView1.Nodes.Add(rootNode);
+            treeView1.ExpandAll();
+            treeView1.SelectedNode = null;
+            
+        }
+        void AddBranch(TreeNode node, DirectoryInfo dir)
+        {
+            var dirNode = new TreeNode(dir.Name, 0, 0);
+            dirNode.Tag = dir.FullName;
+            var dirs = dir.GetDirectories();
+            foreach(var d in dirs)
+            {
+                AddBranch(dirNode, d);
+            }
+            var files = dir.GetFiles();
+            foreach(var f in files)
+            {
+                var fileNode = new TreeNode(f.Name, 1, 1);
+                fileNode.Tag = f.FullName;
+                if(Path.GetExtension(f.Name) == ".zip")
+                {
+                    fileNode.ImageIndex = 2;
+                    fileNode.SelectedImageIndex = 2;
+                }
+                    
+                dirNode.Nodes.Add(fileNode);
+            }
+            node.Nodes.Add(dirNode);
+        }
+     
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            var node = treeView1.SelectedNode;
+            buttonOK.Enabled = false;
+            buttonRename.Enabled = true;
+            buttonDelete.Enabled = true;
+            buttonArchive.Enabled = true;
+            if (node.ImageIndex == 0)
+            {
+                buttonOpen.Enabled = false;
+                buttonDearchive.Enabled = false;
+                buttonCopy.Enabled = false;
+                buttonCreateFile.Enabled = true;
+                buttonCreateFolder.Enabled = true;
+            }
+            if (node.ImageIndex == 1)
+            {
+                buttonOpen.Enabled = true;
+                buttonDearchive.Enabled = false;
+                buttonCopy.Enabled = true;
+                buttonCreateFile.Enabled = false;
+                buttonCreateFolder.Enabled = false;
+            }
+            if (node.ImageIndex == 2)
+            {
+                buttonOpen.Enabled = false;
+                buttonDearchive.Enabled = true;
+                buttonCopy.Enabled = true;
+                buttonCreateFile.Enabled = false;
+                buttonCreateFolder.Enabled = false;
+            }
+           // currentNode = node;
+        }
+        void ResetButtons()
+        {
+            buttonOK.Enabled = false;
+            buttonRename.Enabled = false;
+            buttonDelete.Enabled = false;
+            buttonArchive.Enabled = false;
+            buttonOpen.Enabled = false;
+            buttonDearchive.Enabled = false;
+            buttonCopy.Enabled = false;
+            buttonCreateFile.Enabled = false;
+            buttonCreateFolder.Enabled = false;
+            treeView1.SelectedNode = null;
+            operation = 0;
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            var node = treeView1.SelectedNode;
+            //if (node == null)
+              //  return;
+            var fullName = (string)node.Tag;
+            var name = Path.GetFileName(fullName);
+            var extension = Path.GetExtension(name);
+            var dir = Path.GetDirectoryName(fullName);
+            var nameWithoutExtension = Path.GetFileNameWithoutExtension(name);
+            //var fullNameWithoutExtension = Path.GetFileNameWithoutExtension(fullName);
+            File.Copy(fullName, Path.Combine(dir,nameWithoutExtension + "(1)" + extension), true);
+            ResetButtons();
+
+        }
+
+        private void buttonRename_Click(object sender, EventArgs e)
+        {
+            var node = treeView1.SelectedNode;
+            currentNode = node;
+            if(node.ImageIndex == 0)
+            {
+                textBox1.Text = node.Text;
+            }
+            else
+            {
+                textBox1.Text = Path.GetFileNameWithoutExtension(node.Text);
+            }
+            ResetButtons();
+            operation = 1;
+            buttonOK.Enabled = true;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            if(operation == 1)
+            {
+                var oldName = (string)currentNode.Tag;
+                var newName = textBox1.Text;
+                var dir = Path.GetDirectoryName(oldName);
+                try
+                {
+                    if (currentNode.ImageIndex == 0)
+                    {
+                        var fullName = Path.Combine(dir, newName);
+                        //if (ValidFullName(fullName))
+                            Directory.Move(oldName, fullName);
+                    }
+                    else
+                    {
+                        var extension = Path.GetExtension(oldName);
+                        var fullName = Path.Combine(dir, newName + extension);
+                        while (File.Exists(fullName))
+                        {
+                            newName += "(1)";
+                            fullName = Path.Combine(dir, newName + extension);
+                        }
+                        //if (ValidFullName(fullName))
+                            File.Move(oldName, fullName);
+
+                    }
+                }
+                catch(Exception exeption)
+                { }
+                
+            }
+            ResetButtons();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            var node = tree
+        }
+        //private bool ValidFullName(string name)
+        //{
+        //    if (name.Length >= 256)
+        //        return false;
+        //    return true;
+        //}
+        //private bool ValidName(string name)
+        //{
+        //    if (name.Length == 0)
+        //        return false;
+        //    if (name == "CON" || name == "PRN" || name == "AUX" || name == "NUL" || name == "COM1" || name == "COM2" || name == "COM3" ||
+        //        name == "COM4" || name == "COM5" || name == "COM6" || name == "COM7" || name == "COM8" || name == "COM9" || name == "LPT1" ||
+        //        name == "LPT2" || name == "LPT3" || name == "LPT4" || name == "LPT5" || name == "LPT6" || name == "LPT7" || name == "LPT8" || name == "LPT9")
+        //        return false;
+        //    if (name.Contains(">") || name.Contains("<") || name.Contains(":") || name.Contains("\"") || name.Contains("\\") || name.Contains("/")
+        //        || name.Contains("|") || name.Contains("?") || name.Contains("*") || name.Contains("\n") || name.Contains("\t"))
+        //        return false;
+        //    if(name[name.Length-1] == ' ' || name[name.Length - 1] == '.')
+        //        return false;
+        //    return true;
+        //}
+    }
+}
